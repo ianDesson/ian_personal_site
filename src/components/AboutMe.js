a46efa9a7ab8e0fc35bad8e1b1f2f77674c2f357
ianDesson <ian.desson@outlook.com>
2019-10-16 00:02:23 -0400

    Add The Beginning of About Me Section

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,21 +1,58 @@
-import React from 'react';
-import { Grid, Typography } from '@material-ui/core';
+import React from "react";
+import { Grid, Typography } from "@material-ui/core";
+import {
+  Card,
+  CardMedia,
+  CardActionArea,
+  CardContent
+} from "@material-ui/core";
 
 const AboutMe = () => {
   return (
-    <Grid container>
+    <Grid
+      container
+      direction="column"
+      style={{ marginTop: "1em", textAlign: "center" }}
+    >
       <div>
-        <Typography variant="title">
+        <Typography variant="h2" style={{ fontFamily: "Raleway" }}>
           About Me
         </Typography>
       </div>
-      <div>
-        <Typography variant="subtitle1">
-          Education
-        </Typography>
-      </div>
+      <Grid container direction="row" justify="space-evenly">
+        <Card>
+          <CardActionArea>
+            <CardMedia
+              image={require("../resources/images/education.jpeg")}
+              title="Education"
+              style={{ height: "300px", width: "300px" }}
+            />
+            <CardContent>
+              <Typography variant="h4">Education</Typography>
+              <Typography varaint="body1">
+                I studied at the University of Ottawa.
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+        <Card>
+          <CardActionArea>
+            <CardMedia
+              image={require("../resources/images/work.jpg")}
+              title="Experience"
+              style={{ height: "300px", width: "300px", }}
+            />
+            <CardContent>
+              <Typography variant="h4">Experience</Typography>
+              <Typography variant="body1">
+                I've participated in multiple internships!
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Grid>
     </Grid>
   );
-}
+};
 
 export default AboutMe;
