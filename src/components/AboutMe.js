6a894b8044ced90d954a14dc1d60315c72fa11fd
ianDesson <ian.desson@outlook.com>
2020-04-26 21:52:53 -0400

    Add text for AboutMe and Experience

---
+++ src/components/AboutMe.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const AboutMe = () => {
+
+  return (
+    <div>
+      I'm a software engineering student at the University of Ottawa and I expect to graduate in December 2021!
+      
+      💻 Technology 💻
+      I initially learned to code in Java during High School and have enjoyed using code to solve problems ever since!
+      Next, I learned Python through online courses the summer before university.
+      Lastly, I taught myself JavaScript to make better websites and then learned React to make bigger apps!
+      I have also learned other technologies like Git, PostgreSQL, SASS, Docker, and Express.
+
+      🏡 Hobbies 🏡
+      I enjoy playing 🎮video games🎮 with my friends in my spare time.
+      With sports, I watch 🏎️Formula 1🏎️ racing every race weekend and love to play ⚽soccer⚽
+      Lastly, I do 🏋️‍♂️powerlifting🏋️‍♂️ multiple times a week to stay in shape.
+
+      🇬🇧 Languages 🇫🇷
+      I consider myself bilingual in English and French!
+      I learned French from Grade 3 to 12 in Ontario's French immersion program.
+      During my studies, I took the DELF B2 test, which was the highest level of fluency for grade 12 students!
+      
+    </div>
+  );
+}
+
+export default AboutMe;
---
+++ src/components/Experience.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Experience = () => {
+
+  return (
+    <div>
+      My Resume can be found here or if you want a quick overview:
+
+      Field Effect Software
+      At Field Effect, I mostly did automated testing with Python.
+      My main project was to create a virtual network and test the threat detection capabilities of Covalence.
+      This meant simulating various network attacks and varifying their detection in the product.
+
+      Cision Inc.
+      At Cision, I was the only QA automation developer for our tool called ZEN.
+      ZEN was a REST API for providing analysis on all the published news articles in the world!
+      In my spare time, I also helped work on the REST API using Java.
+
+      Nuance Communications
+      At Nuance, I primarily worked as a Front-End Developer working on Nuance Mix!
+      My regular tasks involved implementing changes to the UI of the Mix Dashboard and NLU tools.
+      I also developed a new library of our React components and its CI/CD in GitLab!
+      Another project I worked on was an app for streaming files to the Mix REST API.
+    </div>
+  );
+}
+
+export default Experience;
