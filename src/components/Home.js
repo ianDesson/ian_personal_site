8b6d4a3ee4587fc130438bbe2a493196188be9a2
Ian Desson <ian.desson@outlook.com>
2021-10-16 21:28:37 -0400

    Remove unused import

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,6 @@ import React from "react";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import FileIcon from "@material-ui/icons/InsertDriveFile";
-import Waves from "./Waves";
 
 import resume from "../resources/ianDessonResume.pdf";
 
